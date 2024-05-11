8d751e3c3247df913eafd7ac93601dcfc2501903
Aurumdev952 <benjamugi20072@gmail.com>
2024-05-11 18:43:29 +0200

    fix

--- backend/src/controllers/verification-controllers.ts
+++ backend/src/controllers/verification-controllers.ts
@@ -1,15 +1,11 @@
-import { updateVerificationSchema } from "@/schema/verification";
 import { verifyVerification } from "@/services/verification-services";
 import { createHandler } from "@/utils/create";
 
-export const handleVerifyVerifcation = createHandler(
-  updateVerificationSchema,
-  async (req, res) => {
-    const id = req.params.id as string;
-    const verification = await verifyVerification({
-      id,
-      isVerified: true,
-    });
-    res.status(200).json(verification);
-  }
-);
+export const handleVerifyVerifcation = createHandler(async (req, res) => {
+  const id = req.params.id as string;
+  const verification = await verifyVerification({
+    id,
+    isVerified: true,
+  });
+  res.status(200).json(verification);
+});
